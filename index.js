22aee5f2c318869feba03a169ea054e52fc2feb9
zhangjun.groove <zhangjun.groove@bytedance.com>
2022-09-25 15:42:17 +0800

    update : 增加本地判断

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const {blogServe} = require('./blogServe');
 
+const argv = process.argv.slice(2)
+
 
 blogServe({
-    location: 'http://groove-zhang.cn/'
+    location: argv.includes('local')?undefined: 'http://groove-zhang.cn/'
 });
